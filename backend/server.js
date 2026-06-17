f46dc5f8a172a2f889d070e12081d09bf503d0fd
Mostafizur01 <mostafizurrahman43@gamil.com>
2026-06-17 16:15:35 +0600

    fix

--- backend/server.js
+++ backend/server.js
@@ -32,18 +32,20 @@ connectMogoose()
 
 let mydata
 
-mydata = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465, 
-    secure: true,
-    auth: {
-        user: 'mostafizurrahmanmd43@gmail.com',
-        pass: process.env.EMAIL_PASS 
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
-})
+try {
+    mydata = nodemailer.createTransport({
+        host: 'smtp-relay.brevo.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.EMAIL_USER, 
+            pass: process.env.EMAIL_PASS  
+        }
+    });
+    console.log("Brevo transporter created successfully.");
+} catch (error) {
+    console.log('Error setting up Brevo transport:', error);
+}
 
 
 app.post('/api/register', async (req, res) => {
